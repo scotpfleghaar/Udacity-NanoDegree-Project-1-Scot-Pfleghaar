149de8f920653f15389357860f842adc4191bc85
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-06-21 09:21:09 -0500

    Adding Commits to media.py

--- media.py
+++ media.py
@@ -5,6 +5,7 @@ class Movie():
     """ This class provides a way to store movie related information"""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
+    # Initialization of class function to display information to web browser
     def __init__(self,
                  movie_title,
                  movie_storyline,
@@ -15,5 +16,6 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # The function that calls the trailer to be opened in the web browser
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
